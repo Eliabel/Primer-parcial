f365c801241c1b1e1b62697d3b2da7e787494872
Eliabel Rosario <amparem09@gmail.com>
2020-06-04 22:14:46 -0400

    first commit

---
+++ Dia de la semana 1er parcial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Parcial_Dia_de_la_semana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dia;
+            Console.WriteLine("Escribe un dia de la semana: ");
+            dia = Console.ReadLine();
+            dia = dia.ToLower();
+            if (dia == "sabado" || dia=="domingo")
+            {
+                Console.WriteLine("Es un fin de semana, disfrutalo. ");
+
+            }
+            else
+            {
+                Console.WriteLine("No es un fin de semana, debes hacer los deberes.");
+            }
+        }
+    }
+}
---
+++ Programa de compra 1er parcial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace aaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p, f;
+            double t;
+            Console.WriteLine("Digite el precio del producto que desea comprar: ");
+            p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("### Ahora ingrese el metodo de pago deseado #### \n EFECTIVO [1]      TARJETA[2] ");
+            f = Convert.ToInt32(Console.ReadLine());
+            switch (f)
+            {
+                case 1: Console.WriteLine("Gracias por su compra, su factura es de $" + p);
+                        break;
+                case 2:
+                    Console.WriteLine("Ingrese los 12 digitos de su tarjeta: ");
+                    t=Convert.ToDouble(Console.ReadLine());
+                    if (t > 99999999999) {
+                    
+                        Console.WriteLine("Su pago se a efectuado correctamente, su factura es de $"+p+" Gracias por su compra.");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hubo un error con los datos de su tarjeta, ingrese de nuevo a la aplicacion y intentelo nuevamente. ");
+
+                    }
+                    break;
+            }
+
+
+        }
+    }
+}
